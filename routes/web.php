e2eab48de9381f5b124202953a461695fecd4ac8
Nathan Zeager <nathanzeager@hotmail.com>
2023-11-09 13:46:34 -0500

    Updated home route, home page style, and displayed database data on page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Models\Link::all();
+
+    return view('welcome', ['links' => $links]);
 });
 
 Auth::routes();
